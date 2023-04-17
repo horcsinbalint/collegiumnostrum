2b9704905d365f358e173671b9538a5de3c163b6
viktorcsimma <csimmaviktor03@gmail.com>
2023-04-17 16:10:45 +0200

    Creating new alumni; validation and toasts not yet working

--- app/Http/Controllers/AlumnusController.php
+++ app/Http/Controllers/AlumnusController.php
@@ -90,6 +90,38 @@ class AlumnusController extends Controller
         return Redirect::route('alumni.show', $alumnus);
     }
 
+    const ALPHABET="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    /**Extracts the rows from the worksheet into an array, from $startingRow (starting from zero) until the end, from the first column until $lastColumn (starting from zero).
+     * $lastColumn must be <26 for now.
+     */
+    public static function worksheet_to_array(\PhpOffice\PhpSpreadsheet\Worksheet\Worksheet $worksheet, int $startingRow, int $lastColumn): array
+    {
+        $highestRow = $worksheet->getHighestRow();
+        $arr=array();
+        $lastColLet = AlumnusController::ALPHABET[$lastColumn];
+        for ($i = $startingRow+1; $i <= $highestRow; ++$i) //indexing starts from 1 here
+        {
+            $arr[] = array_values( //converting to an indexed array; otherwise it would have to be indexed by letters
+                $worksheet->rangeToArray( //appending it to the end
+                    "A$i:$lastColLet$i",     // The worksheet range that we want to retrieve
+                    NULL,        // Value that should be returned for empty cells
+                    TRUE,        // Should formulas be calculated (the equivalent of getCalculatedValue() for each cell)
+                    TRUE,        // Should values be formatted (the equivalent of getFormattedValue() for each cell)
+                    TRUE         // Should the array be indexed by cell row and cell column
+                )[$i] //it would otherwise add an extra dimension
+            );
+        }
+        return $arr;
+    }
+
+    /**
+     * Handles a request with an uploaded worksheet file that contains more than one alumni.
+     * Extracts the data and stores it in new Alumnus objects.
+     * 
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function import_store(Request $request)
     {
         $request->validate(
@@ -99,26 +131,52 @@ class AlumnusController extends Controller
         );
         
         $file = $request->file('file');
+        if (null == $file)
+        {
+            return redirect()->back()->with('message', 'Nincs kiválasztva fájl.');
+        }
         $reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader('Ods'); //TODO: check extension and select based on it
         $reader->setReadDataOnly(true); //we don't care about formatting
-        //TODO: ReadFilter for only the cells in the appropriate rows?
+        //maybe a ReadFilter for only the cells in the appropriate columns?
         $spreadsheet = $reader->load($file->getRealPath());
         $worksheet = $spreadsheet->getActiveSheet();
 
-        //maybe this into a separate function?
-        $highestRow = $worksheet->getHighestRow();
-        $tojson=array();
-        for ($i = 2; $i <= $highestRow; ++$i) //indexing starts from 1 here
+        $rows = AlumnusController::worksheet_to_array($worksheet, 1, 15);
+
+        //TODO: validating data from spreadsheet
+
+        $len = count($rows);
+        if ($len > 0)
         {
-            $tojson[] = $worksheet->rangeToArray( //appending it to the end
-                "A$i:P$i",     // The worksheet range that we want to retrieve
-                NULL,        // Value that should be returned for empty cells
-                TRUE,        // Should formulas be calculated (the equivalent of getCalculatedValue() for each cell)
-                TRUE,        // Should values be formatted (the equivalent of getFormattedValue() for each cell)
-                TRUE         // Should the array be indexed by cell row and cell column
-            )[$i]; //it would otherwise add an extra dimension
+            $firstone = $rows[0][0];
+            foreach ($rows as $row)
+            {
+                $alumnus = Alumnus::factory()->create([
+                    'name' => $row[0],
+                    'email' => $row[1],
+                    'birth_date' => $row[2],
+                    'birth_place' => $row[3],
+                    'high_school' => $row[4],
+                    'graduation_date' => $row[5],
+                    'start_of_membership' => $row[6],
+                    'further_course_detailed' => $row[7],
+                    'recognations' => $row[8],
+                    'research_field_detailed' => $row[9],
+                    'links' => $row[10],
+                    'works' => $row[11],
+                ]);
+                $names[] = $row[0];
+            }
+
+            --$len;
+            //for some reason this does not work
+            return redirect()->route('alumni.index')
+                ->with('message', "Added $firstone and $len others");
+        } else
+        {
+            return redirect()->back()
+                ->with('message', 'A feltöltött fájl nem tartalmaz alumnikat.');
         }
-        return response()->json($tojson);
     }
 
     /**
@@ -129,7 +187,6 @@ class AlumnusController extends Controller
      */
     public function show(Alumnus $alumnus)
     {
-        //
         return view('alumni.show', [
             'alumnus' => $alumnus,
         ]);
