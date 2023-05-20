aaba899f3c0fef3f0ab1a1b2e009771502ac0e15
Horcsin Bálint <horcsinbalint@gmail.com>
2023-05-20 11:26:34 +0200

    #33 Added some translations

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Jelszava helyre lett állítva!',
+    'sent' => 'Elküldtük a jelszó-visszaállítási linket e-mail címére!',
+    'throttled' => 'Kérjük, várjon, mielőtt újra próbálkozik.',
+    'token' => 'Nem megfelelő jelszó-visszaállítási kulcs.',
+    'user' => "A megadott felhasználó nem található.",
 
 ];
